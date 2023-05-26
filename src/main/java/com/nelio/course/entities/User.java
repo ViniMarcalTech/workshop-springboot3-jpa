9e2be982e5be504f024f92abf9d405c949b73d3c
ViniMarcal.tech <vinicius.dm1515@gmail.com>
2023-05-26 12:08:34 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/nelio/course/entities/User.java
+++ src/main/java/com/nelio/course/entities/User.java
@@ -1,11 +1,16 @@
 package com.nelio.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +26,11 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List <Order> orders = new ArrayList<>();    
+
 
     public User(){}
 
@@ -72,6 +82,11 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+    
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -97,5 +112,6 @@ public class User implements Serializable {
         return true;
     }
     
+  
     
 }
