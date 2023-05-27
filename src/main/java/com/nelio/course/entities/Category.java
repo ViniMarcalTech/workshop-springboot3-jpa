057beff16d4650ed0066592407f02b9bb1e9404b
ViniMarcal.tech <vinicius.dm1515@gmail.com>
2023-05-27 16:02:25 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/nelio/course/entities/Category.java
+++ src/main/java/com/nelio/course/entities/Category.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 
 @Entity
@@ -21,8 +23,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
     
     public Category() {}
