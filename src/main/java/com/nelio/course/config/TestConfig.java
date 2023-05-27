377fc3bacba653a87cdf28823548ee34e02e4c9b
ViniMarcal.tech <vinicius.dm1515@gmail.com>
2023-05-27 16:43:32 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/nelio/course/config/TestConfig.java
+++ src/main/java/com/nelio/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.nelio.course.entities.Category;
 import com.nelio.course.entities.Order;
+import com.nelio.course.entities.OrderItem;
 import com.nelio.course.entities.Product;
 import com.nelio.course.entities.User;
 import com.nelio.course.entities.enums.OrderStatus;
 import com.nelio.course.repositories.CategoryRepository;
+import com.nelio.course.repositories.OrderItemRepository;
 import com.nelio.course.repositories.OrderRepository;
 import com.nelio.course.repositories.ProductRepository;
 import com.nelio.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     
     @Override
@@ -72,6 +77,15 @@ public class TestConfig implements CommandLineRunner{
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+
         
     }
 
