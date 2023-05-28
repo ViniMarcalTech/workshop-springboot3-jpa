13ae7737e3ae26813821e419254a6eb06c8ac85a
ViniMarcal.tech <vinicius.dm1515@gmail.com>
2023-05-28 09:59:45 -0300

    User insert - HTTP 201 Created

--- src/main/java/com/nelio/course/repositories/UserRepository.java
+++ src/main/java/com/nelio/course/repositories/UserRepository.java
@@ -3,6 +3,5 @@ package com.nelio.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.nelio.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User,Long> {
-    
+public interface UserRepository extends JpaRepository<User,Long> {    
 }
